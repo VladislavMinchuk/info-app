7c8df25b05bef97f3b7e75b1165b8c2ccce770a9
vladislav <supervladislavgun@gmail.com>
2019-08-18 00:30:57 +0300

    user controller upd

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,18 @@
 const Employees = require('../models/Employees');
 
 module.exports.getPage = (req, res) => {
-  Employees.findAll({
-    raw: true,
-  })
-    .then(items => {
-      res.render('user-add', {
-        empyFields: req.flash('empyFields'),
-        complete: req.flash('completeMsg'),
-      });
-    })
-    .catch(console.log);
+  res.render('user-add', {
+    empyFields: req.flash('empyFields'),
+    complete: req.flash('completeMsg'),
+    badReqForm: req.flash('bodyForm'),
+  });
 };
 
 module.exports.addUser = (req, res) => {
   const { name, surname, age, city, position, cluster } = req.body;
 
   if (req.errorFields.length) {
+    req.flash('bodyForm', req.body);
     req.flash('empyFields', req.errorFields);
     res.redirect('/user');
   } else {
@@ -28,7 +24,7 @@ module.exports.addUser = (req, res) => {
       position: position,
       cluster: cluster,
     })
-      .then(item => {
+      .then(() => {
         req.flash('completeMsg', `User ${name} ${surname} added.`);
         console.log('User added');
         res.redirect('/user');
