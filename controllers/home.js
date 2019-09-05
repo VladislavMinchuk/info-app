adfe0b01ef19eb7a5214b6b76e757548b12358f6
vladislav <supervladislavgun@gmail.com>
2019-09-05 23:44:18 +0300

    database updated

--- controllers/home.js
+++ controllers/home.js
@@ -1,29 +1,32 @@
 const Op = require('Sequelize').Op;
 const Employees = require('../models').employees;
+const Cities = require('../models').cities;
+const Clusters = require('../models').clusters;
+const Positions = require('../models').positions;
 
 module.exports.getIndex = function(req, res) {
   res.render('home', { formData: req.formData });
 };
 
 module.exports.getUsers = function(req, res) {
-  const { name, age, city, cluster } = req.query;
+  const { name, surname, age, city, cluster, position } = req.query;
   let fields = [];
   let where = {};
 
   for (const field in req.query) {
     if (req.query.hasOwnProperty(field)) {
       const value = req.query[field];
-      fields.push(field);
+      fields.push({ [field]: value });
 
-      if (value) {
-        where[field] = {
-          [Op.or]: [value, ''],
-        };
-      }
+      // if (value) {
+      //   where[field] = {
+      //     [Op.or]: [value, ''],
+      //   };
+      // }
     }
   }
 
-  console.log(where);
+  // console.log(fields);
 
   Employees.findAll({
     raw: true,
@@ -32,10 +35,15 @@ module.exports.getUsers = function(req, res) {
         [Op.or]: [name, ''],
       },
     },
-    // attributes: fields,
+    include: [
+      { model: Cities, where: { city: city }, attributes: ['city'] },
+      // { model: Positions, where: { position: position }, attributes: ['position'] },
+      // { model: Clusters, where: { cluster: cluster }, attributes: ['cluster'] },
+    ],
+    attributes: ['id', 'name', 'surname'],
   })
     .then(data => {
-      // console.log(data);
+      console.log(data);
       res.render('home', { formData: req.formData, data });
     })
     .catch(err => {
